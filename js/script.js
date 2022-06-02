d8bf7a13cf228faba061eb205a5506a8b4c15d8d
Nadine Rodriguez <inadinerod@gmail.com>
2022-06-01 20:28:24 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var firstListItem = document.querySelector("ul li");
+console.log(firstListItem);
+
+firstListItem.style.textTransform = "uppercase";
+
+var moreReasons = document.querySelector(".more-reasons");
+console.log(moreReasons);
+
+moreReasons.style.fontSize = "2.5em";
+
+var whyJS = document.querySelector("h3");
+console.log(whyJS);
+
+whyJS.innerHTML = 'Why learn <span class="highlight">JavaScript</span>?';
+console.log(whyJS);
+
+var highlight = document.querySelector(".highlight");
+highlight.style.backgroundColor = "#ffff82";
+
+var mainImg = document.querySelector("img");
+console.log(mainImg);
+
+mainImg.src = "img/js-code.png";
+mainImg.alt = "JavaScript code example";
